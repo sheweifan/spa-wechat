2689f37dabaa85c7ba97c75f6d014672c11610c3
weifan she <923986234@qq.com>
2018-03-26 16:28:57 +0800

    fix wechat controller

--- server/server/controllers/wechat.js
+++ server/server/controllers/wechat.js
@@ -1,49 +1,50 @@
-import { parse as urlParse } from 'url';
-import { parse as queryParse } from 'querystring';
-import * as api from '../api';
-import config from '../config';
+import { parse as urlParse } from 'url'
+import { parse as queryParse } from 'querystring'
+import * as api from '../api'
+import config from '../config'
 
 export async function signature (ctx, next){
-  let url = ctx.query.url;
+  let url = ctx.query.url
   console.log(ctx.query)
-  if(!url) ctx.throw(404);
-
-  url = decodeURIComponent(url);
-
-  const params = await api.getSignaturAsync(url);
-
+  if(!url) ctx.throw(404)
+  url = decodeURIComponent(url)
+  const data = await api.getSignaturAsync(url)
   ctx.body = {
-    params,
+    data,
     success: true
   }
 
 }
 
 export async function redirect (ctx, next){
-  const target = config.SITE_ROOT_URL + '/oauth';
-  const scope = 'snsapi_userinfo';
-  const { a,b } = ctx.query;
-  const params = `${a}_${b}`;
+  const target = config.SITE_ROOT_URL + '/oauth'
+  const scope = 'snsapi_userinfo'
+  const { a = 0, b = 0, to } = ctx.query
+  const params = `${a}_${b}`
+  // const params = ``
 
-  const url = await api.getAuthorizeURL(scope, target, params);
+  const url = await api.getAuthorizeURL(scope, to, params)
 
-
-  ctx.redirect(url);
+  // ctx.redirect(url)
+  ctx.body = {
+    url,
+    success: true
+  }
 }
 
 export async function oauth (ctx, next){
-  let url = ctx.query.url;
-  if(!url) ctx.throw(404);
+  let url = ctx.query.url
+  if(!url) ctx.throw(404)
 
-  url = decodeURIComponent(url);
+  url = decodeURIComponent(url)
 
-  const urlObj = urlParse(url);
+  const urlObj = urlParse(url)
 
-  const { code } = queryParse(urlObj.query);
+  const { code } = queryParse(urlObj.query)
 
-  const user = await api.getUserByCode(code);
+  const user = await api.getUserByCode(code)
 
-  console.log(user);
+  console.log(user)
 
   ctx.body = {
     success: true,
@@ -53,10 +54,10 @@ export async function oauth (ctx, next){
 
 
 export async function getUserByCode (ctx, next){
-  const { code } = ctx.query;
-  const user = await api.getUserByCode(code);
+  const { code } = ctx.query
+  const user = await api.getUserByCode(code)
   ctx.body = {
-    success: true,
+    success: !!user.openid,
     data: user
   }
-  }
+}
