e70d7c627425223a10debe3dfc75c1d6a6f13b89
weifan she <923986234@qq.com>
2018-03-28 14:27:07 +0800

    remove useless dispatch

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view></router-view>
   </div>
 </template>
 
@@ -13,9 +13,6 @@ export default {
     HelloWorld
   },
   created () {
-    this.$store.dispatch('updateUserinfo', {
-      username: 'fuck'
-    })
   }
 }
 </script>
