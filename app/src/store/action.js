578e559de48360a14821d6f662a0c33f9d6a6d11
weifan she <923986234@qq.com>
2018-03-30 09:10:42 +0800

    update store

--- app/src/store/action.js
+++ app/src/store/action.js
@@ -1,4 +1,29 @@
+import axios from 'axios'
+import { prefix } from '@/config'
 
-export const updateUserinfo = ({commit}, userinfo) => {
-  commit('updateUserinfo', userinfo)
+export const updateUserinfo = ({commit}, code) => axios.get(prefix + '/wechat-getUserByCode', {
+  params: { code }
+})
+  .then(res => {
+    const { data, success } = res.data
+    if (success) {
+      commit('updateUserinfo', data)
+      return Promise.resolve(data)
+    } else {
+      return Promise.reject(data)
+    }
+  })
+
+export const redirect = ({commit}, params = {a: 1, b: 2}) => axios.get(prefix + '/wechat-redirect', {
+  params
+})
+  .then(data => {
+    const { url, success } = data.data
+    if (success) {
+      window.location.href = url
+    }
+  })
+
+export const markEntry = ({commit}, url = window.location.href) => {
+  commit('markEntry', url)
 }
